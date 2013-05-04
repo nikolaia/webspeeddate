05c3a5ee9390de27724ffb747a9a4e2073de707a
Nikolai <nikolai@stasj.com>
2013-05-04 12:20:50 +0200

    new get verb
    Express now serve index.html from client on root /

--- webspeeddate.js
+++ webspeeddate.js
@@ -2,6 +2,10 @@ var holla = require('holla');
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res){
+	res.sendfile("./client/index.html");
+});
+
 app.get('/:file', function(req, res){
 	var file = req.params.file
 	res.sendfile("./client/"+file);
