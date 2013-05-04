e570abb4e7adfa9a28873c2942c387eea964ceb9
Nikolai <nikolai@stasj.com>
2013-05-04 11:49:49 +0200

    Fixed faulty merge

--- webspeeddate.js
+++ webspeeddate.js
@@ -1,9 +1,6 @@
 var holla = require('holla');
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app).listen(81);
-var rtc = holla.createServer(server);
-
 var server = require('http').createServer(app).listen(8180);
 var rtc = holla.createServer(server);
 
