168c02c99bc91fcf0522631760bc03240df86bfb
Nikolai <nikolai@stasj.com>
2013-05-04 11:00:48 +0200

    Dependencies and main js file
    Added express as a dependencie
    Simple server now runs

--- webspeeddate.js
+++ webspeeddate.js
@@ -1,1 +1,7 @@
-// Main file
+var holla = require('holla');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app).listen(8080);
+var rtc = holla.createServer(server);
+
+console.log('Server running on port 8080');
